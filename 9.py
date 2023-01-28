b7afb6fe293ddd202f73fc852adac9060482bec5
pradeepben3 <97969637+pradeepben3@users.noreply.github.com>
2023-01-28 20:21:55 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,15 @@
+import cupy as cp
+
+arr=cp.ones(5*10**9,dtype=cp.uint8)
+nn=cp.random.randint(1,10**9,size=10**6)
+import cupy as cp
+from numba import cuda
+                           
+@cuda.jit
+def my_kernel(arr,nn):
+    i = cuda.grid(1)
+    if i<nn.size:
+       # if arr[i] ==1:
+        arr[nn[i]]=0  
+ my_kernel[256,36](arr,nn)
+ print(arr[:100])
