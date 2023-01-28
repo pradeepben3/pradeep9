b2eb742495e5cc5dddb19732da41cffd3c68b239
pradeepben3 <97969637+pradeepben3@users.noreply.github.com>
2023-01-28 20:31:05 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,10 +1,11 @@
 import cupy as cp
 
-arr=cp.ones(5*10**9,dtype=cp.uint8)
-nn=cp.random.randint(1,10**9,size=10**7)
+arr=cp.ones(7*10**9,dtype=cp.uint8)
+nn=cp.random.randint(1,10**9,size=10**8)
 import cupy as cp
+import time
 from numba import cuda
-                           
+start =time.time()                           
 @cuda.jit
 def my_kernel(arr,nn):
     i = cuda.grid(1)
@@ -14,3 +15,4 @@ def my_kernel(arr,nn):
 my_kernel[256,36](arr,nn)
 x=cp.where(arr==0)
 print(x[:10000])
+print(time.time()-start)
